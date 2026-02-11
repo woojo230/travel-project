d85e09abc1dfc8f8b1d3593d42a19cd4cc5cbcfe
woojo230 <woojunghyck@naver.com>
2026-02-11 18:41:11 +0900

    feat: bucketListItem 모듈 설정

--- src/bucket-list-items/bucket-list-items.module.ts
+++ src/bucket-list-items/bucket-list-items.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BucketListItemsService } from './bucket-list-items.service';
 import { BucketListItemsController } from './bucket-list-items.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BucketList } from 'src/bucket-lists/entities/bucketList.entity';
+import { User } from 'src/users/entities/user.entity';
+import { BucketListItem } from './entities/bucketListItem.entity';
+import { Destination } from 'src/destinations/entities/destination.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([BucketList, User, BucketListItem, Destination]),
+  ],
   controllers: [BucketListItemsController],
   providers: [BucketListItemsService],
 })
